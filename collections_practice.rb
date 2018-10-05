cc12cbdaded2ae3e8c11b0b1e82a57884c3aac43
Matt Etress <mattetress@me.com>
2018-10-05 04:46:29 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |x, y|
+    x.length <=> y.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |item|
+    split = item.split("")
+    split[2] = "$"
+    kesha << split.join("")
+  end
+  kesha
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?("a") }
+end
+
+def sum_array(array)
+  array.inject {|a, b| a + b}
+end
+
+def add_s(array)
+  array.each_with_index.collect {|word, index| index == 1 ? word : word << "s"}
+end
